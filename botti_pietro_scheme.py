529b9e1c4af49712cb128ff3af0d835650d0f559
Martin Sandve Alnæs <martinal@simula.no>
2011-09-28 09:51:11 +0200

    Sketch code overview.

--- botti_pietro_scheme.py
+++ botti_pietro_scheme.py
@@ -12,14 +12,41 @@ from dolfin import *
 
 # Parameters
 d, m = 2, 10
+T0, T1 = 0.0, 0.2
+dt = 0.1
 
-# Define geometry
+# Define mesh
 if d == 2:
     mesh = UnitSquare(m, m)
 elif d == 3:
     mesh = UnitCube(m, m, m)
+
+# TODO: Define subdomains
+
+# Get UFL geometric quantities
 cell = mesh.ufl_cell()
 x = cell.x
 n = cell.n
 
-# Define function spaces
+# TODO: Define function spaces
+
+
+# TODO: Define functions
+
+
+# TODO: Define forms
+
+
+# TODO: Define boundary conditions
+
+
+# Run time loop
+t = T0
+while t < T1:
+    # TODO: Solve advection-diffusion equations
+
+    # TODO: Solve pressure equation
+
+    t += dt
+
+# TODO: Postprocessing
